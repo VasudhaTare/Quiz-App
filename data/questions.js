4d769345e8d89e9a3a0977dee91345398bea8904
Vasudha <tarevasudha2202@gmail.com>
2023-05-13 18:29:51 +0530

    first commit

---
+++ data/questions.js
@@ -0,0 +1,134 @@
+
+export default [
+    {
+        question: "Where is sgsits located?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Bhopal",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Gwalior",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Indore",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"Jabalpur",
+            },
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "How many gates are there in sgsits? ",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"6",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"3",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"4",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"5",
+            },
+        ],
+        correctAnswerIndex: 0,
+
+    },
+    {
+        question: "Name of the cafe of the college?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Cafe 100",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Cafe 92",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Cafe 91",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"cafe 80",
+            },
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "How many parkings are there in sgsits?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"20",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"10",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"2",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"23",
+            },
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "How do you feel about college?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"ok",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"bad",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"best",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"good",
+            },
+        ],
+        correctAnswerIndex: 2
+    }
+]
